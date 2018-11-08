37ca997578903ee139700766feb7be41bd556cf8
Joey Clover <joey.clover@gmail.com>
2018-11-08 16:10:51 +0000

    add a log for debugging

--- lookup.go
+++ lookup.go
@@ -3,6 +3,7 @@ package gotwilio
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 )
@@ -73,7 +74,9 @@ type LookupResponse struct {
 // GetLookup uses twillio to get information about a phone number
 // See https://www.twilio.com/docs/lookup/api for more information.
 func (twilio *Twilio) GetLookup(phoneNumber string, lookupOptions *LookupOptions) (lookupResponse *LookupResponse, exception *Exception, err error) {
-	twilioUrl := twilio.LookupUrl + "/v1/PhoneNumbers/" + phoneNumber + ".json"
+	twilioUrl := twilio.LookupUrl + "/v1/PhoneNumbers/" + phoneNumber
+
+	log.Printf("Using URL: %s\n", twilioUrl)
 
 	q := url.Values{}
 
